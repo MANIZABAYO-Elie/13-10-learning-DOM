b84e5049a816ded0b322ab97acc7b9b3949aa730
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-10-13 22:15:29 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const button = document.getElementById('btn');
+const display = document.getElementById('display');
+button.addEventListener('click',function(){
+    display.innerHTML = "Thank you for reaching with us"
+})
