18a0c2f73047bd60f0770e0468148b36c4438577
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-10-14 06:33:14 +0200

    file committed

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const button = document.getElementById('btn');
-const display = document.getElementById('display');
-button.addEventListener('click',function(){
-    display.innerHTML = "Thank you for reaching with us"
-})
+// const button = document.getElementById('btn');
+// const display = document.getElementById('display');
+// button.addEventListener('click',function(){
+//     display.innerHTML = "Thank you for reaching with us"
+// })
