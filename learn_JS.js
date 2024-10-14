7e9b9d483217e17fb3a4c693784d303231c4d0a8
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-10-14 07:06:13 +0200

    file committed

--- learn_JS.js
+++ learn_JS.js
@@ -5,3 +5,8 @@ let  x= 17;
 console.log(x);
 let age = 25;
 console.log(age);
+console.log(typeof age);
+
+let firstName = "Elie";
+console.log(typeof firstName);
+console.log(`my name is ${firstName} and I am ${age} old`);
