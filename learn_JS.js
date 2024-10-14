e5f9384b408ec9a1efed15628076db2c3fd587d6
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-10-14 06:54:09 +0200

    files added

--- learn_JS.js
+++ learn_JS.js
@@ -2,3 +2,6 @@ console.log("hello world");
 // variables ; a variable is a container that stores values
 // declaration and assignment 
 let  x= 17;
+console.log(x);
+let age = 25;
+console.log(age);
