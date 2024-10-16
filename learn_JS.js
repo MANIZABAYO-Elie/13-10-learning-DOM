ba4de1cdaa55ac39d6b94f4b8a7d2c6e2b000ba0
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-10-16 20:14:52 +0200

    file committed

--- learn_JS.js
+++ learn_JS.js
@@ -1,35 +1,35 @@
-console.log("hello world");
-// variables ; a variable is a container that stores values
-// declaration and assignment 
+// console.log("hello world");
+// // variables ; a variable is a container that stores values
+// // declaration and assignment 
 
-let  x= 17;
-console.log(x);
-let age = 25;
-console.log(age);
-console.log(typeof age);
+// let  x= 17;
+// console.log(x);
+// let age = 25;
+// console.log(age);
+// console.log(typeof age);
 
-let firstName = "Elie";
-console.log(typeof firstName);
-console.log(`my name is ${firstName} and I am ${age} old`);
-let online = true;
-console.log(typeof online);
+// let firstName = "Elie";
+// console.log(typeof firstName);
+// console.log(`my name is ${firstName} and I am ${age} old`);
+// let online = true;
+// console.log(typeof online);
 
-// Arthmetic operators
+// // Arthmetic operators
 
-let students = 30;
-students += 1;
-console.log(students);
+// let students = 30;
+// students += 1;
+// console.log(students);
 
-// accepting user input
-//1.window prompt
-//2.professional way or HTML textbox
+// // accepting user input
+// //1.window prompt
+// //2.professional way or HTML textbox
 
-// Type conversion 
-let num = 123;
-console.log(typeof num);
-// Math properties
-let c= 2.155;
-let e = 3;
-  f= Math.round(c);
-  console.log(f);
+// // Type conversion 
+// let num = 123;
+// console.log(typeof num);
+// // Math properties
+// let c= 2.155;
+// let e = 3;
+//   f= Math.round(c);
+//   console.log(f);
 
