dc128199ef53f2797377ca3051cd7c1046ac6b2e
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-10-24 21:55:29 +0200

    file committed

--- script.js
+++ script.js
@@ -3,10 +3,10 @@
 // button.addEventListener('click',function(){
 //     display.innerHTML = "Thank you for reaching with us"
 // })
-document.getElementById('my_H1').textContent = 'LEARNING JAVASCRIPT';
-let fullName = "MANIZABAYO Elie";
-document.getElementById('p1').textContent = fullName;
-let age = 26;
+// document.getElementById('my_H1').textContent = 'LEARNING JAVASCRIPT';
+// let fullName = "MANIZABAYO Elie";
+// document.getElementById('p1').textContent = fullName;
+// let age = 26;
 
 
  const button = document.getElementById('myButton');
