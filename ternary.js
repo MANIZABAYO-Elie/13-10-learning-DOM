d4a1cac732289200e5ce87aa2141e2ce1e60c978
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-10-30 22:08:08 +0200

    file committed

--- ternary.js
+++ ternary.js
@@ -1,5 +1,8 @@
 // ternary operator 
 
-let age = 21;
- let message = age>=18 ? "you are an adult": "you are an adult";
-console.log(message);
+// let age = 21;
+//  let message = age>=18 ? "you are an adult": "you are an adult";
+// console.log(message);
+let time = 16;
+let greeting = time<12 ? "Good morning" :"Good afternoon";
+console.log(greeting);
