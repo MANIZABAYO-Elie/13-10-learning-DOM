23f4dc91ed98fd21c291e3732a6af22a2cbb00b1
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-10-31 22:44:09 +0200

    filr committed

--- ternary.js
+++ ternary.js
@@ -3,9 +3,9 @@
 // let age = 21;
 //  let message = age>=18 ? "you are an adult": "you are an adult";
 // console.log(message);
-let time = 16;
-let greeting = time<12 ? "Good morning" :"Good afternoon";
-console.log(greeting);
-let isStudent = false;
-let message = isStudent ? "you are student":"you are not student";
-console.log(message);
+// let time = 16;
+// let greeting = time<12 ? "Good morning" :"Good afternoon";
+// console.log(greeting);
+// let isStudent = false;
+// let message = isStudent ? "you are student":"you are not student";
+// console.log(message);
