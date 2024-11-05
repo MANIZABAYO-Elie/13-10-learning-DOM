f559dac56b793f7e1901d6799bb6dbb85ba80ede
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-11-05 21:54:14 +0200

    file committed

--- learn_JS.js
+++ learn_JS.js
@@ -82,6 +82,13 @@
 //     console.log("this pi");
 // }
  for (let i = 0; i<=4; i++) {
-console.log(i);
+console.log("hello world");
     
  }
+ for (let i = 1; i<=20; i++) {
+    if(i===13){
+
+        continue;
+    }
+ }   
+     
