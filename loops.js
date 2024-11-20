20ab53e5d3b3bacc7374ebe9c20d5f551127efac
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-11-20 11:52:53 +0200

    file committed

--- loops.js
+++ loops.js
@@ -4,12 +4,21 @@
 //     const elements = numbers[i];
 //     console.log(elements);
 //  }
-const numbers = [1,2,3,4,5]
-  function findSum(numbers){
-    let sum = 0;
-    for(let i = 0 ; i< numbers.length ; i++){
-        sum += numbers[i];
+// const numbers = [1,2,3,4,5]
+//   function findSum(numbers){
+//     let sum = 0;
+//     for(let i = 0 ; i< numbers.length ; i++){
+//         sum += numbers[i];
+//     }
+//     return sum ;
+// }
+// console.log(findSum(numbers));
+const array = [1,2,3,4,9,0,6,7,11,13,17,15,20,22,26,25];
+const newArray = []
+for (let i = 0; i < array.length; i++) {
+    if(i<15) {
+array.push(i);
     }
-    return sum ;
+    return array;
 }
-console.log(findSum(numbers));
+console.log(array);
