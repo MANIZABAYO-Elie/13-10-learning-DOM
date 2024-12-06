dd6a2beb77b598fb099f50a6371678fcb887cbb0
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-12-06 22:15:03 +0200

    file commirtted

--- loops.js
+++ loops.js
@@ -45,3 +45,11 @@ for (let i = 0; i <5; i++) {
     
 }
 console.log(ourArray);
+
+let cheese = "Cheddar";
+
+if (cheese) {
+  console.log("Yay! Cheese available for making cheese on toast.");
+} else {
+  console.log("No cheese on toast for you today.");
+}
